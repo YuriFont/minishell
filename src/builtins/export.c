997721abfdbf4fa9aa258076090a4a00cd977211
erickramosxp <erickramossilva@gmail.com>
2024-06-14 19:18:09 -0300

    add export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -2,28 +2,58 @@
 
 int	verify_nome_of_variable_is_valid(char *name)
 {
-	(void)name;
+	char	*not_valid;
+	int		i;
+	int		j;
+
+	not_valid = NO_VALID_CHAR;
+	i = 0;
+	while (name[i])
+	{
+		j = 0;
+		while (not_valid[j])
+		{
+			if (name[i] == not_valid[j])
+			{
+				printf("export: `%s': not a valid identifier\n", name);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
 	return (1);
 }
 
-int	valid_new_variable(char *new_variable)
+char	*valid_new_variable(char *new_variable)
 {
 	char	*find_equal;
 	char	*name_variable;
 
 	find_equal = ft_strchr(new_variable, '=');
 	if (!find_equal)
-		return (0);
+		return (NULL);
 	name_variable = ft_substr(new_variable, 0,
 			ft_strlen(new_variable) - ft_strlen(find_equal));
-	(void)name_variable;
-	return (1);
+	if (!verify_nome_of_variable_is_valid(name_variable))
+	{
+		free(name_variable);
+		return (NULL);
+	}
+	return (name_variable);
 }
 
 void	insert_in_env(t_env_list *env, t_token *token)
 {
-	(void)env;
+	char	*name;
+	char	*value;
+
 	if (!token)
 		return ;
-	valid_new_variable(token->text);
+	name = valid_new_variable(token->text);
+	if (!name)
+		return ;
+	value = ft_strchr(token->text, '=') + 1;
+	add_new_variable(env, name, value);
+	free(name);
 }
